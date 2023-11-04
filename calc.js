ef2f349f2987a2909f70709fcbda90937589a672
Gilmar Alberto <gilabreu00@gmail.com>
2023-11-04 10:12:37 -0300

    curso de nodejs

---
+++ calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./somar")
+var SubFunc = require("./subtrair")
+var MultFunc = require("./multiplicar")
+var DivFunc = require("./dividir")
+
+console.log(SomaFunc(1,2))
+console.log(SubFunc(9,3))
+console.log(MultFunc(3,5))
+console.log(DivFunc(25,5))
---
+++ desafio002.js
@@ -0,0 +1,4 @@
+let nome = "Gilmar";
+let sobrenome = "Pinto";
+
+console.log(` ${nome} ${sobrenome}`)
